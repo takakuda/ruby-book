39ab0e35a31154103e8077ef7206b88c5dcc34f1
kei <kei.takakuda.0415@icloud.com>
2019-04-24 02:17:37 +0900

    convert_hash_syntax test

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -3,6 +3,20 @@ require '../lib/convert_hash_syntax'
 
 class ConvertHashSyntaxTest < Minitest::Test
   def test_convert_hash_syntax
-    assert_equal '{}', convert_hash_syntax('{}')
+    old_syntax = <<~TEXT
+      {
+        :name => 'kei',
+        :age => 20,
+        :gender => :male
+      }
+    TEXT
+    expected = <<~TEXT
+      {
+        name: 'kei',
+        age: 20,
+        gender: :male
+      }
+    TEXT
+    assert_equal expected, convert_hash_syntax(old_syntax)
   end
 end
